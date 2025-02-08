37097ce2d0e467a4d1d549c0e6e016e8af5fbf19
root <nigelsaurino@gmail.com>
2025-02-08 13:43:49 -0600

    Taking my first crack in a while at a toy orderbook

--- include/Orderbook.hpp
+++ include/Orderbook.hpp
@@ -4,27 +4,27 @@
 #include <map>
 
 enum Side{
-    Bid,
-    Ask
+    BID,
+    ASK
 };
 
 using Price = int;
 using OrderId = int;
-using Volume = int; 
-
-
+using Quantity = int; 
 
 struct Order{
     static int order_count_;
     OrderId order_id_{};
-    Volume volume_{};    
-    Side size_{};
+    Price price_{};
+    Quantity quantity_{};    
+    Side side_{};
 };
+
 
 
 class Orderbook{
 public:
-    Orderbook(){
+    Orderbook(): orders_{}, asks_{}, bids_{}{
         
     }
     
@@ -33,15 +33,48 @@ public:
     }
     
     void Add(const Order& order){
+        if(orders_.count(order.order_id_)){
+            return;
+        }
         
+        if(order.side_ == BID){
+            bids_[order.price_].emplace_back(order);
+            orders_[order.order_id_] = --bids_[order.price_].end();
+        }else{
+            asks_[order.price_].emplace_back(order);
+            orders_[order.order_id_] = --asks_[order.price_].end();
+        } 
+        
+        Match();
     }
     
     void Cancel(const Order& order){
+        if(!orders_.count(order.order_id_)){
+            return;
+        }
+        
+        auto order_it{orders_[order.order_id_]};
 
+        if(order.side_ == BID){
+            bids_[order.price_].erase(order_it);
+        }else{
+            asks_[order.price_].erase(order_it);
+        }
+
+        orders_.erase(order.order_id_);
     }
     
     void Modify(const Order& old_order, const Order& modify_order){
-        
+        if(orders_.count(old_order.order_id_)){
+            Cancel(old_order);
+        } 
+        Add(modify_order);
+        Match();
+    }
+    
+    bool Match(){
+
+        return false;
     }
     
 private:
